9e35b9589577d5323cb83f7c0501c531c823018e
Zhasmin <zh_suleimenova@kbtu.kz>
2023-01-22 22:13:21 +0600

    pp2-22B030444

--- String Method1.py
+++ String Method1.py
@@ -1,5 +1,60 @@
-txt = "python is FUN!"
+txt = "Hello, welcome to my world."
 
-x = txt.capitalize()
+x = txt.endswith(".")
 
-print (x)
+print(x)
+
+txt = "H\te\tl\tl\to"
+
+x =  txt.expandtabs(2)
+
+print(x)
+
+txt = "H\te\tl\tl\to"
+
+print(txt)
+print(txt.expandtabs())
+print(txt.expandtabs(2))
+print(txt.expandtabs(4))
+print(txt.expandtabs(10))
+
+txt = "Hello, welcome to my world."
+
+x = txt.find("welcome")
+
+print(x)
+
+
+txt = "Hello, welcome to my world."
+
+x = txt.index("welcome")
+
+print(x)
+
+txt = "Hello, welcome to my world."
+
+x = txt.index("e", 5, 10)
+
+print(x)
+
+txt = "Company12"
+
+x = txt.isalnum()
+
+print(x)
+
+txt = "Company 12"
+
+x = txt.isalnum()
+
+print(x)
+txt = "CompanyX"
+
+x = txt.isalpha()
+
+print(x)
+txt = "Company123"
+
+x = txt.isascii()
+
+print(x)
