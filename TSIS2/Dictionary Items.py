9003eb9354f38d06d528afb9b01e10a74a5eb053
Zhasmin2004 <zh_suleimenova@kbtu.kz>
2023-01-30 20:49:28 +0600

    lab2

---
+++ TSIS2/Dictionary Items.py
@@ -0,0 +1,47 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+x = thisdict.get("model")
+print(x)
+x = thisdict.keys()
+
+print(x)
+x = thisdict.values()
+
+print(x)
+x = thisdict.items()
+
+print(x)
+
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
+x = car.values()
+
+print(x) #before the change
+
+car["year"] = 2020
+
+print(x) #after the change
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
